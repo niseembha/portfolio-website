a57b32c338f853626d5874b285f6fbca8463c120
MonkeNiseem <niseem2008@gmail.com>
2026-06-30 17:03:47 -0500

    added Deep Work Scoreboard

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import DeepWorkScoreboard from './DeepWorkScoreboard.jsx'
+
+const basePath = import.meta.env.BASE_URL.replace(/\/$/, '')
+const pathname = window.location.pathname
+  .slice(basePath.length)
+  .replace(/\/+$/, '') || '/'
+const rootPage = pathname === '/deep-work-scoreboard' ? <DeepWorkScoreboard /> : <App />
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {rootPage}
   </StrictMode>,
 )
